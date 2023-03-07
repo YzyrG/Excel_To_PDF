bd38a37d1851fedf4f9712dc71a1aeaaad9fc61e
Zyr <zyr724725@gmail.com>
2023-03-07 10:32:17 +0800

    load excel files into dataframes

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 """
 Convert Excel files to PDF
 """
+import pandas
+import glob
+import openpyxl
+
+
+# 将多个文件路径读入list:filepaths
+filepaths = glob.glob("excels/*.xlsx")
+# print(filepaths)
+
+for filepath in filepaths:
+    content = pandas.read_excel(filepath, sheet_name="Sheet 1")
+    print(content)
+
+
+
+
+
 
